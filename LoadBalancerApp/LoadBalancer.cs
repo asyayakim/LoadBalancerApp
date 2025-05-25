d3a675e6ff2120c793da53c22fc78eb5405eb736
asyayakim <asyayakimova@yahoo.com>
2025-05-25 17:41:03 +0200

    init

---
+++ LoadBalancerApp/LoadBalancer.cs
@@ -0,0 +1,25 @@
+using LoadBalancerApp;
+
+public class LoadBalancer
+{
+    private List<Server> servers = new List<Server>();
+    private int lastUsedServer = -1;
+    public void RegisterServer(Server server)
+    {
+        servers.Add(server);
+    }
+    public Server GetServerRoundRobin()
+    {
+        lastUsedServer = (lastUsedServer + 1) % servers.Count;
+        return servers[lastUsedServer];
+    }
+    public Server GetServerLeastConnections()
+    {
+        return servers.OrderBy(s => s.RequestCount).First();
+    }
+    public Server GetServerIpHashing(string ipAddress)
+    {
+        int index = Math.Abs(ipAddress.GetHashCode()) % servers.Count;
+        return servers[index];
+    }
+}
---
+++ LoadBalancerApp/Server.cs
@@ -0,0 +1,16 @@
+namespace LoadBalancerApp;
+
+public class Server
+{
+    public string Id { get; }
+    public int RequestCount { get; private set; }
+    public Server(string id)
+    {
+        Id = id;
+        RequestCount = 0;
+    }
+    public void HandleRequest()
+    {
+        RequestCount++;
+    }
+}
